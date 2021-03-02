89e2aca1ea4d8d12fa2c97c8323fa784381556b0
Debarun Roy <debarun.dr7.roy@gmail.com>
2021-03-02 13:32:53 +0530

    Del: deleted Custom notes array made for testing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "./notes";
+
 import CreateArea from "./CreateArea";
 function App() {
   
@@ -10,9 +10,7 @@ function App() {
     <div className="App">
       <Header/>
       <CreateArea/>
-      {notes.map((note) => 
-  <Note key={note.key} title={note.title} Content={note.Content} />
-  )}
+      
       <Footer/>
     </div>
   );
