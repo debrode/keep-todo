8b2e5f063727b3559a9e63e0574fbdccb290ebf4
Debarun Roy <debarun.dr7.roy@gmail.com>
2021-03-02 17:23:59 +0530

    Style Fix: Added Nav Icon

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,13 @@
 import React from "react";
-
+import HighlightIcon from "@material-ui/icons/Highlight";
 function Header(){
     return (
        <header>
-           <h1>To-Do-Keeper</h1>
+           
+           <h1>
+           <HighlightIcon />
+               To-Do-Keeper
+               </h1>
        </header>
     );
 }
