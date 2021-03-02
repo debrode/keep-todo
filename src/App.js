a76260b1d2bd1fc90239654b40c52e2b725e5dcb
Debarun Roy <debarun.dr7.roy@gmail.com>
2021-03-02 13:47:17 +0530

    fix: fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React,{useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 
 import CreateArea from "./CreateArea";
 function App() {
+  const [notes,setNote]=useState([]);
   function addNote(note) {
-    
+    setNote(prev=>{
+      return [...prev,note];
+    });
   }
   return (
     <div className="App">
       <Header/>
       <CreateArea onAdd={addNote} />
-      
+      {notes.map((note)=>{
+         return <Note 
+          title={note.title}
+          Content={note.Content}
+         />
+      })}
       <Footer/>
     </div>
   );
