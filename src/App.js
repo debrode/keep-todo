b357639242f3246fa4df173e0450b2060dda52e2
Debarun Roy <debarun.dr7.roy@gmail.com>
2021-07-19 08:06:37 +0530

    Feat Add: storing values in the localstorage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React,{useState} from "react";
+import React,{useState,useEffect} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 
 import CreateArea from "./CreateArea";
+
+const Local_Key="keepTODO.storage.note";
 function App() {
   const [notes,setNote]=useState([]);
   function addNote(note) {
@@ -18,6 +20,16 @@ function App() {
       });
     });
   }
+  useEffect(()=>{
+    const noteJSON=localStorage.getItem(Local_Key);
+    if(noteJSON!=null) setNote(JSON.parse(noteJSON));
+  },[])
+
+  useEffect(()=>{
+   localStorage.setItem(Local_Key,JSON.stringify(notes));
+  },[notes])
+
+
   return (
     <div className="App">
       <Header/>
